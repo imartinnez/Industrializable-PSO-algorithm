73e2b3c0ef87b32347b58ae8dd6e6d445cd064af
Iñigo <martinezinigo34@gmail.com>
2026-03-04 19:02:02 +0100

    PSO engine

--- pso/core/pso.py
+++ pso/core/pso.py
@@ -20,7 +20,7 @@ class PSO:
     
     def generate_particles(self):
         particles = []
-        for i  in range(self.n_particles):
+        for _  in range(self.n_particles):
             position = np.array([np.random.uniform(min_i, max_i) for (min_i, max_i) in self.constraints])
             velocity = np.array([np.random.uniform(-abs(max_i-min_i), abs(max_i-min_i)) for (min_i,max_i) in self.constraints])
             value = self.fitness_f(position)
@@ -29,14 +29,45 @@ class PSO:
             particles.append(p)
     
         self.swarm = s.Swarm(particles)
+        self.swarm.update_b_global()
+    
+    def run(self):
+        self.generate_particles()
+        np.random.seed(42)
+        seeds = np.random.randint(1, 2**30, self.max_iter)
+        b_global = float("inf")
         
-    def evaluate():
-        pass
+        for i in range(self.max_iter):
+            
+            seed = seeds[i]
+            np.random.seed(seed)
+            
+            for p in self.swarm.particles:   
+                
+                value = self.fitness_f(p.position)
+                p.update_best_value(value)
+            
+            self.swarm.update_b_global()
+            
+            for p in self.swarm.particles:   
+                
+                r1 = np.random.rand()
+                r2 = np.random.rand()
+                p.update_velocity(self.w, self.c1, self.c2, r1, r2, self.swarm.b_gposition)
+                p.update_position()
+                
+            if abs(b_global - self.swarm.b_gvalue) < self.tol:
+                break
+            b_global = self.swarm.b_gvalue
+            
 
+        
+        
+    
 class PSOSequential(PSO):
-    def evaluate():
+    def evaluate(self):
         pass
 
 class PSOParallel(PSO):
-    def evaluate():
+    def evaluate(self):
         pass
